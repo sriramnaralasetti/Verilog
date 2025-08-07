e35d2146c457d10caafc3a7ec384b613da9f8b49
sriramnaralasetti <sriramnaralasetti999@gmail.com>
2025-08-07 20:12:29 +0530

    Add files via upload

---
+++ 4-Bit Up counter/count.v
@@ -0,0 +1,18 @@
+module count(input [3:0]din,
+                    input clk,rst,
+                    input load,
+                    output reg [3:0]count
+                   );
+always@(posedge clk)
+begin
+if(rst)
+count<=4'b0000;
+else if(load==1)
+count<=din;
+else
+count<=count+4'b0001;
+end
+endmodule
+
+
+
